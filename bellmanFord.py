2ef60ed713b594f901e3b8d54583d9c3d05b2bd8
Darren <darrenzhang200027@gmail.com>
2021-04-25 16:31:12 -0400

    finished bellmanFord

--- bellmanFord.py
+++ bellmanFord.py
@@ -40,22 +40,70 @@ class Graph:
             if vertice not in self.graph:
                 self.graph[vertice] = Vertice(vertice, float('inf'))
         
+        # assuming edges don't have duplicates
+        for edge, cost in edges:
+            current, neighbor = edge
+            neighborVertice = self.graph[neighbor]
+            self.graph[current].neighbors.append((neighborVertice, cost))
+
+        self.applyBellmanFord(source, len(verticies))
+
+    def applyBellmanFord(self, source, numVerticies):
         if source not in self.graph:
             raise Exception("Source not in graph")
+        
+        sourceVertice = self.graph[source]
+        sourceVertice.cost = 0
 
-        # assuming edges don't have duplicates
-        for current, neighbor in edges:
-            neighborVertice = self.graph[neighbor]
-            self.graph[current].neighbors.append(neighborVertice)
+        # perform v - 1 relaxations
+        for _ in range(numVerticies):
+            self.performRelaxations()
+
+        # detect negative cycle
+
+    def performRelaxations(self):
+        for key in self.graph.keys():
+            vertice = self.graph[key]
+
+            if vertice.cost == float('inf'):
+                continue
+            
+            for neighbor, cost in vertice.neighbors:
+                # print("---- upating ----", "key", key, "neighbor", neighbor.name, "prev cost", neighbor.cost, "new comp", vertice.cost + cost)
+                neighbor.cost = min(neighbor.cost, vertice.cost + cost)
+
+    def containsNegativeCycle(self):
+        for key in self.graph.keys():
+            vertice = self.graph[key]
+
+            if vertice.cost == float('inf'):
+                continue
+            
+            for neighbor, cost in vertice.neighbors:
+                if vertice.cost + cost < neighbor.cost:
+                    return True
+
+        return False
+
+
     def printGraph(self):
         for vertice in self.graph.keys():
             print("Vertice: ", vertice)
-            for neighbor in self.graph[vertice].neighbors:
-                print("Neighbor", neighbor.name)
+            for neighbor, cost in self.graph[vertice].neighbors:
+                print("Neighbor", neighbor.name, "cost", neighbor.cost)
             print("\n")
 
+    def printCosts(self):
+        for key in self.graph:
+            vertice = self.graph[key]
+            print(vertice.name, vertice.cost)
+
 v = [1, 2, 3, 4, 5, 6, 7]
-e = [(1, 2), (1, 3), (1, 4), (2, 5), (3, 5), (4, 3), (4, 6), (5, 7), (6, 7)]
-g = Graph(v, e, 1)
-g.printGraph()
+e = [[(1, 2), 6], [(1, 3), 5], [(1, 4), 5], [(2, 5), -1], [(3, 2), -2], [(3, 5), 1], [(4, 3), -2], 
+[(4, 6), -1], [(5, 7), -3], [(6, 7), 3]]
+e2 = [[(1, 2), 4], [(1, 4), 5], [(2, 4), 5], [(3, 2), -10], [(4, 3), 3]]
+
+g = Graph(v, e2, 1)
+g.printCosts()
+print(g.containsNegativeCycle())
 
