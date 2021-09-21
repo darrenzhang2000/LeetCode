844b077005457ababa158e9a04509f07b7ac8699
Darren Zhang <44158788+darrenzhang2000@users.noreply.github.com>
2021-09-21 19:50:22 -0400

    Create minKnightMoves.py

---
+++ minKnightMoves.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minKnightMoves(self, x: int, y: int) -> int:
+        @lru_cache
+        def dfs(x, y):
+            if x + y == 0:
+                return 0
+            elif x + y == 2:
+                return 2
+            else:
+                return min(dfs(abs(x - 1), abs(y - 2)), dfs(abs(x - 2), abs(y - 1))) + 1
+            
+        return dfs(abs(x), abs(y))
